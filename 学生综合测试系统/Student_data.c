cf1651ed19e41e8f63201b621a951c116b95b640
郑杨阳 <3102473691@qq.com>
2023-12-20 02:36:43 +0800

    2023.12.20

--- 学生综合测试系统/Student_data.c
+++ 学生综合测试系统/Student_data.c
@@ -1,6 +1,21 @@
 #include "config.h"
 #define table_capacity 50
 #define stu_path "Student.txt"
+
+Student* initialize_table() {//建立链表
+	static Student* head;//存放链表的头节点
+	Student* local = (Student*)malloc(sizeof(Student));//local为链表的中间节点，此行生成的指针为链表的头节点
+	head = local;
+	for (int i = 0; i < table_capacity - 1; i++) {
+		local->next = (Student*)malloc(sizeof(Student));//建立中间节点，并把他们都连起来，形成链表
+		local = local->next;
+	}
+	local->next = head;//让链表的尾节点与头节点连接起来，形成循环链表
+	return head;//返回链表的头节点
+}
+
+
+
 void input(Student* stu) {
 	printf("请输入学生的学号：");
 	scanf("%s", stu->stu_num);
@@ -22,7 +37,7 @@ void input(Student* stu) {
 
 
 
-int create_basic() {
+void create_basic() {
 	FILE* p_ini = fopen("basic.csv", "r");
 	if (p_ini == 0) {
 		p_ini = fopen("basic.csv", "w");
@@ -33,14 +48,15 @@ int create_basic() {
 	}
 	Student *new=(Student*)malloc(sizeof(Student));
 	input(new);
-	printf("%s",new->stu_name);
-	fputs(strcat(new->stu_num,","),p_ini);
+	setbuf(stdin, NULL);//使stdin输入流由默认缓冲区转为无缓冲区,达到清除缓存区的目的；
+	fputs(strcat(new->stu_num, ","), p_ini);
 	fputs(strcat(new->stu_name, ","), p_ini);
 	fputs(strcat(new->stu_sex, ","), p_ini);
 	fputs(strcat(new->stu_address, ","), p_ini);
 	fputs(strcat(new->stu_phone_num, "\n"), p_ini);
 	fclose(p_ini);
 }
+
 
 
 
@@ -81,19 +97,9 @@ int create_basic() {
 //
 //
 //
-// student *initialize_table() {
-//	static student* head;
-//	student* local=(student*)malloc(sizeof(student));
-//	head = local;
-//	for (int i = 0; i < table_capacity-1; i++) {
-//		local->next= (student*)malloc(sizeof(student));
-//		local = local->next;
-//	}
-//	local->next = head;
-//	return head;
-//}
-//
-//
-//
+
+
+
+
 
 
--- 学生综合测试系统/Student_data.h
+++ 学生综合测试系统/Student_data.h
@@ -1,6 +1,6 @@
 #ifndef __STUDENT_DATA_H__
 #define __STUDENT_DATA_H__
-int create_basic();
+void create_basic();
 
 #endif // !__STUDENT_DATA_H__
 
