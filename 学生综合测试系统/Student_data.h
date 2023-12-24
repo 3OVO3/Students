df7f03e902e4069038059d5249fa03a62e15ae22
89484902 <89484902@qq.com>
2023-12-24 20:03:36 +0800

    11

--- 学生综合测试系统/Student_data.h
+++ 学生综合测试系统/Student_data.h
@@ -1,14 +1,25 @@
 #ifndef __STUDENT_DATA_H__
 #define __STUDENT_DATA_H__
-Student* initialize_table();
+
+#include <conio.h>//包含_getch()函数的头文件，仅在本函数集使用
+
+//功能函数【5】
 void create_basic();
-void get_data(Student* head);
-Student* sort_link_table(Student* head);
-void browse_data();
 void alter_basic();
-void renew_data(Student* head);
 void delete_basic();
+void select_basic();
+void browse_data();
+
+//读写函数【2】
+Student* initialize_table();
+void renew_data(Student* head);
+
+//子函数【4】
+Student* sort_link_table(Student* head);
+void get_data(Student* head);
 int browe_center(int col_width, int len);
+void table_head_input(Student* head);
+
 #endif // !__STUDENT_DATAs_H__
 
 
